fdaf144d124f69dd2c28bf2a727d930db6b9e2f9
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-02-08 00:01:42 -0500

    Update data.js

--- data.js
+++ data.js
@@ -34,16 +34,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.28103495173983184,
-          "pitch": -0.3832827901914797,
+          "yaw": 0.20950249137904464,
+          "pitch": -0.4250589715753428,
           "rotation": 0,
           "target": "1-2-c-cha-gisu-ln-tri"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.2359941005558408,
-          "pitch": -0.5836971401848849,
+          "yaw": 0.16823973861483843,
+          "pitch": -0.6735388679733862,
           "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Ngôi mộ trống</span><o:p></o:p></p>",
           "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Sau ngày sa-bát, khi ngày thứ nhất trong tuần vừa ló rạng, bà Ma-ri-a Mác-đa-la và một bà khác cũng tên là Ma-ri-a, đi viếng mộ. Thình lình, đất rung chuyển dữ dội: thiên thần Chúa từ trời xuống, đến lăn tảng đá ra, rồi ngồi lên trên; diện mạo người như ánh chớp, và y phục trắng như tuyết. Thấy người, lính canh khiếp sợ, run rẩy chết ngất đi. Thiên thần lên tiếng bảo các phụ nữ: “Này các bà, các bà đừng sợ! Tôi biết các bà tìm Đức Giê-su, Đấng bị đóng đinh. Người không có ở đây, vì Người đã trỗi dậy như Người đã nói. Các bà đến mà xem chỗ Người đã nằm, rồi mau về nói với môn đệ Người như thế này: Người đã trỗi dậy từ cõi chết, và Người đi Ga-li-lê trước các ông. Ở đó, các ông sẽ được thấy Người. Đấy, tôi xin nói cho các bà hay.” Các bà vội vã rời khỏi mộ, tuy sợ hãi nhưng cũng rất đỗi vui mừng, chạy về báo tin cho môn đệ Đức Giê-su hay.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Đức Giê-su hiện ra với các phụ nữ</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Bỗng Đức Giê-su đón gặp các bà và nói: “Chào chị em!” Các bà tiến lại gần Người, ôm lấy chân, và bái lạy Người. Bấy giờ, Đức Giê-su nói với các bà: “Chị em đừng sợ! Về báo cho anh em của Thầy để họ đến Ga-li-lê. Họ sẽ được thấy Thầy ở đó.”<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(Mátthêu 28:1–10)</div>"
         }
@@ -83,22 +83,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.5988572128802403,
-          "pitch": -0.3146852861276983,
+          "yaw": 0.8295190152248875,
+          "pitch": -0.16886677939099215,
           "rotation": 0,
           "target": "2-3-c-cha-thnh-thn-hin-xung"
         },
         {
-          "yaw": -2.373600770491757,
-          "pitch": 0.1601391871236295,
+          "yaw": -2.3393628150142955,
+          "pitch": 0.12362877656975968,
           "rotation": 0,
           "target": "0-1-c-cha-gisu-sng-li"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.6056426498458709,
-          "pitch": -0.5744207015345992,
+          "yaw": 0.7809236815983347,
+          "pitch": -0.5434597421898903,
           "title": "Đức Chúa Giêsu Lên Trời",
           "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Lời tựa</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Thưa ngài Thê-ô-phi-lô, trong quyển thứ nhất, tôi đã tường thuật tất cả những việc Đức Giê-su làm và những điều Người dạy, kể từ đầu cho tới ngày Người được rước lên trời. Trước ngày ấy, Người đã dạy bảo các Tông Đồ mà Người đã tuyển chọn nhờ Thánh Thần. Người lại còn dùng nhiều cách để chứng tỏ cho các ông thấy là Người vẫn sống sau khi đã chịu khổ hình: trong bốn mươi ngày, Người đã hiện ra nói chuyện với các ông về Nước Thiên Chúa. Một hôm, đang khi dùng bữa với các Tông Đồ, Đức Giê-su truyền cho các ông không được rời khỏi Giê-ru-sa-lem, nhưng phải ở lại mà chờ đợi điều Chúa Cha đã hứa, “điều mà anh em đã nghe Thầy nói tới, đó là: ông Gio-an thì làm phép rửa bằng nước, còn anh em thì trong ít ngày nữa sẽ chịu phép rửa trong Thánh Thần”.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Thăng thiên</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Bấy giờ những người đang tụ họp ở đó hỏi Người rằng: “Thưa Thầy, có phải bây giờ là lúc Thầy khôi phục vương quốc Ít-ra-en không?” Người đáp: “Anh em không cần biết thời giờ và kỳ hạn Chúa Cha đã toàn quyền sắp đặt, nhưng anh em sẽ nhận được sức mạnh của Thánh Thần khi Người ngự xuống trên anh em. Bấy giờ anh em sẽ là chứng nhân của Thầy tại Giê-ru-sa-lem, trong khắp các miền Giu-đê, Sa-ma-ri và cho đến tận cùng trái đất.”</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Nói xong, Người được cất lên ngay trước mắt các ông, và có đám mây quyện lấy Người, khiến các ông không còn thấy Người nữa. Và đang lúc các ông còn đăm đăm nhìn lên trời phía Người đi, thì bỗng có hai người đàn ông mặc áo trắng đứng bên cạnh và nói: “Hỡi những người Ga-li-lê, sao còn đứng nhìn trời? Đức Giê-su, Đấng vừa lìa bỏ các ông và được rước lên trời, cũng sẽ ngự đến y như các ông đã thấy Người lên trời.”<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(Công Vụ Tông Đồ 1:1-11)</div>"
         }
@@ -138,22 +138,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.07424260601359478,
-          "pitch": -0.04374053474917616,
+          "yaw": -0.07506402957144687,
+          "pitch": -0.10977309799296009,
           "rotation": 0,
           "target": "3-4-c-cha-tri-cho-c-b-ln-tri"
         },
         {
-          "yaw": 1.9070775338421306,
-          "pitch": -0.06402283509622286,
+          "yaw": 1.8835112084244283,
+          "pitch": -0.03659732029855434,
           "rotation": 0,
           "target": "1-2-c-cha-gisu-ln-tri"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.11489246256091334,
-          "pitch": -0.2967560164160066,
+          "yaw": -0.11496180811354506,
+          "pitch": -0.37224441421833276,
           "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Ngày lễ Ngũ Tuần</span><o:p></o:p></p>",
           "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Khi đến ngày lễ Ngũ Tuần, mọi người đang tề tựu ở một nơi, bỗng từ trời phát ra một tiếng động, như tiếng gió mạnh ùa vào đầy cả căn nhà, nơi họ đang tụ họp. Rồi họ thấy xuất hiện những hình lưỡi giống như lưỡi lửa tản ra đậu xuống từng người một. Và ai nấy đều được tràn đầy ơn Thánh Thần, họ bắt đầu nói các thứ tiếng khác, tùy theo khả năng Thánh Thần ban cho.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Lúc đó, tại Giê-ru-sa-lem, có những người Do-thái sùng đạo, từ các dân thiên hạ trở về. Nghe tiếng ấy, có nhiều người kéo đến. Họ kinh ngạc vì ai nấy đều nghe các ông nói tiếng bản xứ của mình. Họ sửng sốt, thán phục và nói: “Những người đang nói đó không phải là người Ga-li-lê cả ư? Thế sao mỗi người chúng ta lại nghe họ nói tiếng mẹ đẻ của chúng ta? Chúng ta đây, có người là dân Pác-thi-a, Mê-đi, Ê-lam, Mê-xô-pô-ta-mi-a, Giu-đê, Cáp-pa-đô-ki-a, Pon-tô, và A-xi-a, có người là dân Phy-ghi-a, Pam-phy-li-a, Ai-cập, và những vùng Li-by-a giáp giới Ky-rê-nê; nào là những người từ Rô-ma đến đây; nào là người Do-thái cũng như người đạo theo; nào là người đảo Cơ-rê-ta hay người Ả-rập, vậy mà chúng ta đều nghe họ dùng tiếng nói của chúng ta mà loan báo những kỳ công của Thiên Chúa!” Ai nấy đều sửng sốt và phân vân, họ bảo nhau: “Thế nghĩa là gì?” Nhưng người khác lại chế nhạo: “Mấy ông này say bứ rồi!”<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(Công Vụ Tông Đồ 2:1-13)</div>"
         }
@@ -193,22 +193,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.19775488367314153,
-          "pitch": -0.17835862533089042,
+          "yaw": 0.28290339959050925,
+          "pitch": -0.3424279478755068,
           "rotation": 0,
           "target": "4-5-c-cha-tri-thng-c-m-trn-tri"
         },
         {
-          "yaw": 2.6015218460469374,
-          "pitch": 0.07325865397914555,
+          "yaw": 2.3791351372577685,
+          "pitch": 0.184547861773261,
           "rotation": 0,
           "target": "2-3-c-cha-thnh-thn-hin-xung"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.18674157142539016,
-          "pitch": -0.36909155406290495,
+          "yaw": 0.2414294338391887,
+          "pitch": -0.5338737219185532,
           "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Bài ca “Ngợi Khen” (Magnificat)</span><o:p></o:p></p>",
           "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Bấy giờ bà Ma-ri-a nói:</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Linh hồn tôi ngợi khen Đức Chúa, thần trí tôi hớn hở vui mừng vì Thiên Chúa, Đấng cứu độ tôi.&nbsp;</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Phận nữ tỳ hèn mọn, Người đoái thương nhìn tới; từ nay, hết mọi đời sẽ khen tôi diễm phúc.&nbsp;</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Đấng Toàn Năng đã làm cho tôi biết bao điều cao cả, danh Người thật chí thánh chí tôn!</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Đời nọ tới đời kia, Chúa hằng thương xót những ai kính sợ Người.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Chúa giơ tay biểu dương sức mạnh, dẹp tan phường lòng trí kiêu căng.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Chúa hạ bệ những ai quyền thế, Người nâng cao mọi kẻ khiêm nhường.&nbsp;</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Kẻ đói nghèo, Chúa ban của đầy dư, người giàu có, lại đuổi về tay trắng.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Chúa độ trì Ít-ra-en, tôi tớ của Người, như đã hứa cùng cha ông chúng ta, vì Người nhớ lại lòng thương xót dành cho tổ phụ Áp-ra-ham và cho con cháu đến muôn đời.”<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(Luca 1:46-55)</div>"
         }
@@ -248,16 +248,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.8704399973105463,
-          "pitch": 0.14422061100579953,
+          "yaw": 2.749964359811174,
+          "pitch": 0.045768055644552774,
           "rotation": 0,
           "target": "3-4-c-cha-tri-cho-c-b-ln-tri"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.7175153663211784,
-          "pitch": -0.21625723866430846,
+          "yaw": 0.6304062715925411,
+          "pitch": -0.20865712269064218,
           "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Người Phụ Nữ và Con Mãng Xà</span><o:p></o:p></p>",
           "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Rồi có điềm lớn xuất hiện trên trời: một người Phụ Nữ, mình khoác mặt trời, chân đạp mặt trăng, và đầu đội triều thiên mười hai ngôi sao. Bà có thai, đang kêu la đau đớn và quằn quại vì sắp sinh con. Lại có điềm khác xuất hiện trên trời: đó là một Con Mãng Xà, đỏ như lửa, có bảy đầu và mười sừng, trên bảy đầu đều có vương miện. Đuôi nó quét hết một phần ba các ngôi sao trên trời mà quăng xuống đất. Rồi Con Mãng Xà đứng chực sẵn trước mặt người Phụ Nữ sắp sinh con, để khi bà sinh xong là nó nuốt ngay con bà. Bà đã sinh được một người con, một người con trai, người con này sẽ dùng trượng sắt mà chăn dắt muôn dân. Con bà được đưa ngay lên Thiên Chúa, lên tận ngai của Người. Còn người Phụ Nữ thì trốn vào sa mạc; tại đó Thiên Chúa đã dọn sẵn cho bà một chỗ ở, để bà được nuôi dưỡng ở đó, trong vòng một ngàn hai trăm sáu mươi ngày.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Bấy giờ, có giao chiến trên trời: thiên thần Mi-ca-en và các thiên thần của người giao chiến với Con Mãng Xà. Con Mãng Xà cùng các thiên thần của nó cũng giao chiến. Nhưng nó không đủ sức thắng được, và cả bọn không còn chỗ trên trời nữa. Con Mãng Xà bị tống ra, đó là Con Rắn xưa, mà người ta gọi là ma quỷ hay Xa-tan, tên chuyên mê hoặc toàn thể thiên hạ; nó bị tống xuống đất, và các thiên thần của nó cũng bị tống xuống với nó. Và tôi nghe có tiếng hô to trên trời:</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">“Thiên Chúa chúng ta thờ giờ đây ban ơn cứu độ, giờ đây biểu dương uy lực với vương quyền, và Đức Ki-tô của Người giờ đây cũng biểu dương quyền bính, vì kẻ tố cáo anh em của ta, ngày đêm tố cáo họ trước tòa Thiên Chúa, nay bị tống ra ngoài. Họ đã thắng được nó nhờ máu Con Chiên và nhờ lời họ làm chứng về Đức Ki-tô: họ coi thường tính mạng, sẵn sàng chịu chết. Nào thiên quốc cùng chư vị ở chốn thiên đình, hãy vì đó mà mừng vui hoan hỷ! Khốn cho đất và biển, vì ma quỷ đã xuống với các ngươi, nó giận điên lên vì biết rằng nó chỉ còn một ít thời gian.”</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">Khi Con Mãng Xà thấy mình đã bị tống xuống đất, nó liền đuổi bắt người Phụ Nữ đã sinh con trai. Bà được ban cho đôi cánh đại bàng, để bay vào sa mạc, vào nơi dành cho bà, tại đó bà được nuôi dưỡng một thời, hai thời và nửa thời, ở xa Con Rắn. Từ miệng, Con Rắn phun nước ra đằng sau bà như một dòng sông, để cuốn bà đi. Nhưng đất cứu giúp bà: đất há miệng ra uống cạn dòng sông từ miệng Con Mãng Xà phun ra. Con Mãng Xà nổi giận với người Phụ Nữ và đi giao chiến với những người còn lại trong dòng dõi bà, là những người tuân theo các điều răn của Thiên Chúa và giữ lời chứng của Đức Giê-su. Rồi nó đứng trên bãi cát ngoài biển.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(Khải Huyền 12:1-18)</div>"
         }
